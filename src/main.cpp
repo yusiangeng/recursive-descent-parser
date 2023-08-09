d103eb93772464d719c4cd002a34910a29dfc20b
Joe <engyusiang@icloud.com>
2023-08-09 17:57:00 +0800

    Accept CLI arguments

--- src/main.cpp
+++ src/main.cpp
@@ -8,21 +8,37 @@
 
 using json = nlohmann::json;
 
-int main() {
-  Parser parser;
-
-  std::string program;
-
-  std::ifstream file;
-  file.open("program.txt", std::ios::in);
-  if (file.is_open()) {
-    char c;
-    while (file.get(c)) {
-      program += c;
-    }
-    file.close();
+int main(int argc, char *argv[]) {
+  if (argc < 3) {
+    std::cout << "Usage:\n"
+                 "  bin/parse -e '<your_program>'\n"
+                 "  bin/parse -f path/to/file"
+              << std::endl;
+    return 1;
   }
 
+  std::string mode = argv[1];
+  std::string exp = argv[2];
+
+  std::string program;
+  if (mode == "-e") {
+    program = exp;
+  } else if (mode == "-f") {
+    std::ifstream file;
+    file.open("program.txt", std::ios::in);
+    if (file.is_open()) {
+      char c;
+      while (file.get(c)) {
+        program += c;
+      }
+      file.close();
+    }
+  } else {
+    std::cout << "Invalid mode: has to be either -e or -f" << std::endl;
+    return 1;
+  }
+
+  Parser parser;
   Ast ast;
   try {
     ast = parser.parse(program);
